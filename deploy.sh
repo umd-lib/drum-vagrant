fdb0ebab07b4f0deba7f9908e5a4e57f8600ea58
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2015-05-29 22:15:56 -0400

    Updates for LIBCIR-11 and LIBCIR-39.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cd /apps/mdsoar/tomcat
+ ./control stop
+
 # deploy DSpace
 cd /apps/mdsoar/dspace-installer
 ant update_local
 
 cd /apps/mdsoar/tomcat
-./control restart
+ ./control start 
